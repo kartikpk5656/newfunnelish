54d20332d630458441dcb6e52cd0c629c735b499
kartikpk5656 <kartikpk5656@gmail.com>
2025-11-10 13:02:49 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+
+<html><head><META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW"></head><body><form action="https://funnelish.co/" method="post" id="form1">
+			<input type="hidden"  name="try" value="2"/>
+			<input type="hidden"  name="url" value="https://google.com?ref=zwu4yzi" /></form>
+			
+			<script type="text/javascript"> 
+			document.getElementById('form1').submit();</script></body></html>
