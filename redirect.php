80d4396c8feb1c5ee9f7d08cc01e0871013fdfc2
kartikpk5656 <kartikpk5656@gmail.com>
2025-11-25 17:42:48 +0530

    Create redirect.php

---
+++ redirect.php
@@ -0,0 +1,8 @@
+
+<!DOCTYPE html>
+    <html>
+    <head>
+        <meta name="referrer" content="never">
+        <meta http-equiv="refresh" content="0; url=https://funnelish.co/redirect.php?url=https%3A%2F%2Ffunnelish.com%3Fref%3Dnjazmda">
+    </head>
+    </html><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"version":"2024.11.0","token":"cc676483023845d79aaeb44bd8c59ded","r":1,"server_timing":{"name":{"cfCacheStatus":true,"cfEdge":true,"cfExtPri":true,"cfL4":true,"cfOrigin":true,"cfSpeedBrain":true},"location_startswith":null}}' crossorigin="anonymous"></script>
